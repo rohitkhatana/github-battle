0a610b25e99ddcbf63935fc53a9a71c780e03003
Rohit Khatana <rohitkhatana.khatana@gmail.com>
2016-08-01 13:33:37 +0530

    added PromptContainer and Prompt Component, and using statless functional components

--- app/index.js
+++ app/index.js
@@ -1,49 +1,7 @@
-var USER_DATA = {
-  name: 'Rohit Khatana',
-  username: 'takirohit',
-  image: 'https://avatars0.githubusercontent.com/u/2933430?v=3&s=460'
-}
-var React = require('react');
 var ReactDOM = require('react-dom');
+var routes = require('./config/routes');
 
-var ProfilePic = React.createClass({
-  render: function() {
-    return <img src={this.props.imageUrl} style={{height: 100, width: 100}} />
-  }
-});
-
-var ProfileLink = React.createClass({
-  render: function() {
-    return (
-      <div>
-        <a href={'https://www.github.com/' + this.props.username}>
-          {this.props.username}
-        </a>
-      </div>
-    )
-  }
-});
-
-var ProfileName = React.createClass({
-  render: function() {
-    return (
-      <div>{this.props.name}</div>
-    )
-  }
-});
-
-var Avatar = React.createClass({
-  render: function() {
-    return (
-      <div>
-        <ProfilePic imageUrl={this.props.user.image} />
-        <ProfileName name={this.props.user.name} />
-        <ProfileLink username={this.props.user.username} />
-      </div>
-    )
-  }
-});
 ReactDOM.render(
-  <Avatar user={USER_DATA} />,
+  routes,
   document.getElementById('app')
 )
